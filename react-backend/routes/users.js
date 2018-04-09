5111f1b6965808c3cb58428b345047fcc6e0b969
Jacob Pelletier <jacob.pelletier@pondurance.com>
2018-04-09 17:44:36 -0400

    Backend + Wifi Scanner + Tab Title
    Adjusted backend, npm install to load wifi scanner, and tab title changed

--- react-backend/routes/users.js
+++ react-backend/routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
+var scanner = require('node-wifi-scanner');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  //res.send('respond with a resource');
+
+  scanner.scan((err, networks) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(networks);
+  });
+
   // And insert something like this instead:
   res.json([{
   	id: 1,
